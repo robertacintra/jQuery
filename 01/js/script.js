083772ebb994cedddb3ec8cca7c45627f10ad8ab
robertacintra <roo.moony@gmail.com>
2019-05-13 15:09:51 -0300

    jquery aula 01

---
+++ 01/js/script.js
@@ -0,0 +1,25 @@
+// Javascript
+// var lis = document.querySelectorAll('li');
+// lis.forEach (function(item){
+//     item.innerHTML = "dhjksldsd";
+// })
+
+// $('li').text('shfsdjfksfs');
+
+// function mudaTextoLi(){
+//     $('li').text("jdfhsldkjfhlds");
+// }
+// $(document).ready(function(){
+//     alert("Carregou");
+//     mudaTextoLi();
+// });
+
+// function mudaTextoLi(){
+//     if ($('li').length > 0){
+//         console.log($('.lista1 .botao').text());
+//     }
+// }
+
+// $(document).ready(function(){
+//     mudaTextoLi();
+// })
