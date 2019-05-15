6d4a7bd4f7b905c668991b3d2ded18e058e9cd31
robertacintra <roo.moony@gmail.com>
2019-05-15 15:44:05 -0300

    [AULA 3] api json javascript & jquery

---
+++ 03/AJAX/arquivo/js/script.js
@@ -0,0 +1,22 @@
+function carregaCarta(){
+    $.getJSON("tarot.json", trocaCartaAleatoria);
+}
+
+function trocaCartaAleatoria(cartas, status){
+    if(status == 'success'){
+        //console.log(cartas);
+        let numeroAleatorio = Math.floor(Math.random() * cartas.length);
+        $(".titulo").text(cartas[numeroAleatorio].nome);
+        $(".tipo").text(cartas[numeroAleatorio].tipo);
+        $(".descricao").text(cartas[numeroAleatorio].descricao);
+        $('.image').attr("src", cartas[numeroAleatorio].imagem);
+        $('.saiba-mais').attr("href", cartas[numeroAleatorio].link);
+    } else {
+        $('.message').text("Não foi possível carregar a carta.");
+    }
+}
+
+$(document).ready(function(){
+    carregaCarta();
+    $('#novaCarta').click(carregaCarta);
+})
